96a2c5db64d3d85b44ecc0aac7d39922629dd137
Jason Herrera <baytownjason@yahoo.com>
2021-01-17 09:43:01 -0600

    commit tree 5770afb7938d03f0096a204a5ae8bd22481275ce

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
-## code your solution here. 
+require 'pry'
+# code your solution here. 
+class Cat
+    attr_accessor :name
+   
+    #binding.pry
+    def initialize
+        @name = name
+    end
+    def meow
+        puts "meow!"
+    end
+    def whoami
+        self
+    end
+end
